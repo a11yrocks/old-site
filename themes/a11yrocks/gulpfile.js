6ccfa067dd23d5c5fdcbbee92ef33999f9c8576b
Lucas <lucas@agtlucas.com>
2016-04-15 18:48:23 -0300

    Update gulpfile

---
+++ themes/a11yrocks/gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp')
+const cssnano = require('cssnano')
+const mqpacker = require('css-mqpacker')
+const scss = require('postcss-scss')
+const nested = require('postcss-nested')
+const cssnext = require('postcss-cssnext')
+const atImport = require('postcss-import')
+const postcss = require('gulp-postcss')
+
+gulp.task('css', () => {
+  var processors = [
+    atImport,
+    cssnext({ browsers: ['last 1 version'] }),
+    nested,
+    mqpacker,
+    cssnano()
+  ]
+  return gulp.src('assets/styles/main.css')
+    .pipe(postcss(processors, { syntax: scss }))
+    .pipe(gulp.dest('build/css'))
+})
+
+gulp.task('default', ['css'])
